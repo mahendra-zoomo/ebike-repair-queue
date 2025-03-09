d7a3e21d114f2656139ccdda72ad25ec5b354f76
mahendra-zoomo <mahendra.chouhan@ridezoomo.com>
2025-03-09 00:02:52 +0000

    Add missing index.js and App.js files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CustomerQueue from "./pages/CustomerQueue";
+import TrackRepair from "./pages/TrackRepair";
+import AdminPage from "./pages/AdminPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CustomerQueue />} />
+        <Route path="/track/:id" element={<TrackRepair />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
